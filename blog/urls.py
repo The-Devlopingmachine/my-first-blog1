84cbe5b27d860a4516c8f32768f93a1c4508f1d1
Chirag Sharma <chiragsharmachaahat@gmail.com>
2021-04-07 00:02:39 +0530

    Adding views and forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
+    path('posts/new', views.post_new, name = 'new_post'),
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail')
 ]
